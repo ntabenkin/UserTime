acafb9299b26ef4d7aa6ddf45fa0cacf35a5fdb1
matthewmurphy <MatthewTimothyMurphy@gmail.com>
2022-02-28 14:52:15 -0500

    UP

--- app/src/main/java/com/example/usertime/dropdownmenu/Components.kt
+++ app/src/main/java/com/example/usertime/dropdownmenu/Components.kt
@@ -1,12 +1,10 @@
 package com.example.usertime.dropdownmenu
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
@@ -24,26 +22,21 @@ fun ProfileDropDown() {
 fun ExposedDropDownMenu(stateHolder: ExposedDropMenuStateHolder) {
     Column {
         Box {
+            Modifier.wrapContentSize(Alignment.TopEnd)
             Icon(
                 painter = painterResource(id = stateHolder.icon),
                 contentDescription = null,
-                modifier = Modifier.clickable {
-                    stateHolder.onEnabled(!(stateHolder.enabled))
-                }
+                modifier = Modifier
+                    .clickable { stateHolder.onEnabled(!(stateHolder.enabled)) }
                     .size(30.dp)
             )
-               /* modifier = Modifier.onGloballyPositioned {
-                    stateHolder.onSize(it.size.toSize())
-                }*/
 
             DropdownMenu(
                 expanded = stateHolder.enabled,
                 onDismissRequest = {
                     stateHolder.onEnabled(false)
                 },
-                modifier = Modifier
-                    .width(with(LocalDensity.current)
-                    { stateHolder.size.width.toDp() })
+                modifier = Modifier.wrapContentSize(Alignment.TopEnd)
             ) {
                 stateHolder.items.forEachIndexed { index, s ->
                     DropdownMenuItem(
